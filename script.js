7006061522a6109be3abb1bafed2a05c347bf952
alireza hassanzadeh <AHB.mahdiyar@gmail.com>
2014-06-14 22:29:19 +0430

    Define keys

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 (function(ng, $, undef){
   var
+
+  config = function(){
+    duration: 90 // zaman bazi be saniye
+  },
+
   log = function(){
     console.log.apply(console, arguments);
   },
+
   shortkey = function($event){
-    log($event);
+    switch($event.keyCode){
+      case 32:
+        startGame();
+        break;
+      case 82:
+        resetGame();
+        break;
+      case 49:
+        playerWin(1);
+        break;
+      case 50:
+        playerWin(2);
+        break;
+      default:
+        log('key "%s" not defined.', $event.keyCode);
+    }
   },
+
+  startGame = function(){
+    log('Start game.');
+  },
+
+  resetGame = function(){
+    log('Reset game.');
+  },
+
+  playerWin = function(n){
+    log('Player %s win.', n);
+  },
+
   mainCtrl = function($scope){
     $scope.shortkey = shortkey;
   },
+
   init = function(){
     ng
       .module('taGame', [])
